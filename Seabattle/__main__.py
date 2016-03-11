dd81f0b316e6dec93280af78724619b6e668b24c
kirillsavelyev <kirillsavelyev@yandex.ru>
2016-03-11 15:02:11 +0300

    Seabattle v.2

---
+++ Seabattle/__main__.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from collections import OrderedDict
+
+from classes import *
+from base import *
+
+
+class GameController(object):
+    SHIP_RULES = OrderedDict([(4, 1), (3, 2), (2, 3), (1, 4)])
+
+    def __init__(self):
+        self.players = []
+        player1 = Player()
+        self.players.append(player1)
+        player2 = Player()
+        self.players.append(player2)
+
+        self.field = Field(10)
+
+        for player in self.players:
+            player.create_ships(self.SHIP_RULES, self.field)
+
+    def check_game_status(self):
+        for player in self.players:
+            if all(not ship.afloat for ship in player.ships):
+                return False
+        return True
+
+    def start_game(self):
+        next_shooter = False
+
+        shooter, enemy = self.players
+
+        # начало стрельбы
+        try:
+            while self.check_game_status():
+                if next_shooter:
+                    shooter, enemy = enemy, shooter
+                    self.field.display_shooter_field(shooter)
+
+                self.field.display_enemy_field(shooter)
+                shot = shooter.shooting(self.field, enemy.ships)
+                if shot.hit:
+                    ship = enemy.take_a_hit(shot)
+                    if not ship.afloat:
+                        print('\n', 'Ship had been destroyed!')
+                next_shooter = not shot.hit
+        except KeyboardInterrupt:
+            print('Error')
+
+
+if __name__ == '__main__':
+    game = GameController() # add parameter
+    game.start_game()
---
+++ Seabattle/base.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+__author__ = 'savelyevka'
+
+
+def get_input(message):
+    try:
+        input_function = raw_input
+    except NameError:
+        input_function = input
+
+    return input_function(message)
