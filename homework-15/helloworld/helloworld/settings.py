5a8d05bbcb8a613a3be42c1bd3bc36fd04791b37
Kirill Savelyev <kirillsavelyev@yandex.ru>
2016-04-15 10:27:45 +0300

    homework-15

--- homework-15/helloworld/helloworld/settings.py
+++ homework-15/helloworld/helloworld/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static', 'bower_components', 'bootstrap', 'dist', 'css'),
+    os.path.join(BASE_DIR, 'static', 'bower_components'),
     os.path.join(BASE_DIR, 'static')
 ]
 
